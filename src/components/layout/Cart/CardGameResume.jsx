006488e974b6aaaff0af6cdc62234d506afdb1a5
Guilherme Freire <guilhermefreire841@gmail.com>
2023-07-28 22:53:59 -0300

    :construction: Feat: implementando carrinho

--- src/components/layout/Cart/CardGameResume.jsx
+++ src/components/layout/Cart/CardGameResume.jsx
@@ -1,53 +1,41 @@
+import Utils from "../../../scripts/Utils";
 
-function CardGameResume() {
+function CardGameResume({game, removeItemCart}) {
 
-  const game = {
-    "id": 1,
-    "attributes": {
-        "name": "Far Cry 6",
-        "createdAt": "2023-06-10T23:21:03.119Z",
-        "updatedAt": "2023-07-16T14:06:17.663Z",
-        "publishedAt": "2023-06-10T23:21:12.730Z",
-        "description": "Far Cry 6 é um jogo de tiro em primeira pessoa de mundo aberto desenvolvido pela Ubisoft. Situado em uma ilha fictícia chamada Yara, inspirada na cultura e história de Cuba, o jogo transporta os jogadores para um cenário de revolução e resistência contra um regime ditatorial.",
-        "current_price": 225.24,
-        "last_price": 375.4,
-        "image_url": "https://cdn1.epicgames.com/b4565296c22549e4830c13bc7506642d/offer/TETRA_PREORDER_STANDARD%20EDITION_EPIC_Store_Portrait_1200x1600-1200x1600-ca8b802ff13813c37a44ebf68d0946a2.png",
-        "developer": " Ubisoft",
-        "publisher": "Ubisoft Entertainment",
-        "size": 60,
-        "launch_date": "2021-10-07",
-        "metacritic_score": 74,
-        "measurement": "GB",
-        "edition": "Standard edition",
-        "classification": 18,
-        "thumb": "https://image.api.playstation.com/vulcan/ap/rnd/202012/1522/MEtJOQHXbVy0ux0Emo9HInke.jpg",
-        "short_description": null,
-        "highlighted": false,
-        "popularity": 0.05,
-        "platform": "Windows"
-    }
-  };
+  const utils = new Utils();
 
   return (
-    <div className="bg-stone-950 flex w-4/6 mb-5 m-auto p-5 rounded-xl">
-      <img className="w-2/12 rounded-lg" src={game.attributes.image_url} alt="game" />
-      <div className="pl-5 pr-5 w-2/3">
-        <h4 className="font-bold text-2xl mb-2">{ game.attributes.name }</h4>
-        <p className="bg-stone-700 w-2/4 rounded-lg pb-0.5 mb-2 text-xs text-center font-semibold">{ game.attributes.edition }</p>
-        <p className="text-xs font-semibold">{ game.attributes.platform }</p>
-      </div>
-      <div className="flex flex-col justify-between">
-        <a href="#" className="text-stone-400 font-semibold text-end">Remover</a>
-        <div className="inline-flex items-end">
-          <div className="mr-3">
-            <p className="text-stone-400 line-through">R${ game.attributes.last_price }</p>
-            <p className="font-semibold text-xl">R${ game.attributes.current_price }</p>
+    game && (
+      <div className="bg-stone-950 flex w-4/6 mb-5 m-auto p-5 rounded-xl">
+        <img className="w-2/12 h-32 rounded-lg" src={game.attributes.image_url} alt="game" />
+        <div className="pl-5 pr-5 w-2/3">
+          <h4 className="font-bold text-xl mb-2">{ game.attributes.name }</h4>
+          {
+            game.attributes.edition && (
+              <p className="bg-stone-700 w-2/4 rounded-lg pb-0.5 mb-2 text-xs text-center font-semibold">{ game.attributes.edition }</p>
+            )
+          }
+          <p className="text-xs font-semibold">{ game.attributes.platform }</p>
+        </div>
+        <div className="flex flex-col justify-between">
+          <a onClick={() => removeItemCart(game.id)} href="#" className="text-stone-400 font-semibold text-end">Remover</a>
+          <div className="inline-flex items-end">
+            <div className="mr-3">
+              <p className="text-stone-400 line-through">{ `${utils.getMonetaryFormat(game.attributes.last_price)}` }</p>
+              <p className="font-semibold text-xl">{ `${utils.getMonetaryFormat(game.attributes.current_price)}` }</p>
+            </div>
+           {
+            utils.getDiscount(game.attributes.current_price, game.attributes.last_price) > 0 && (
+              <span className="bg-gradient-to-r from-violet-600 to-blue-600 rounded-xl pt-1 pr-3 pb-1 pl-3
+                            text-center font-semibold">
+              { `-${utils.getDiscount(game.attributes.current_price, game.attributes.last_price)}%` }
+              </span>
+            )
+           }
           </div>
-          <span className="bg-gradient-to-r from-violet-600 to-blue-600 rounded-xl pt-1 pr-3 pb-1 pl-3
-                          text-center font-semibold">-30%</span>
         </div>
       </div>
-    </div>
+    )
   );
 }
 
--- src/components/view/Cart.jsx
+++ src/components/view/Cart.jsx
@@ -1,29 +1,118 @@
+import { useEffect, useState } from "react";
 import CardGameResume from "../layout/Cart/CardGameResume";
 import Footer from "../layout/Footer/Footer";
 import Navbar from "../layout/Navbar/Navbar";
+import axios from "axios";
+import Utils from "../../scripts/Utils";
 
 function Cart() {
+
+  const [cartItems, setCartItems] = useState(null);
+  const [checkout, setCheckout] = useState(null);
+  const utils = new Utils();
+
+  useEffect(() => {
+    if (!cartItems) {
+      getCartItems();
+    }
+    calculateCheckout();
+  }, [cartItems]);
+
+  function getCartItems() {
+    let sessionCart = sessionStorage.getItem("cart");
+    if (sessionCart) {
+      let cart = JSON.parse(sessionCart);
+      if (cart.length) {
+        let url = `${process.env.REACT_APP_API_URL}/api/games?`;
+        let index = 0;
+        cart.forEach(function (cartItem) {
+          url = url.concat(index ? "&":"", `filters[id][$eq][${index}]=${cartItem.id}`)
+          index++;
+        })
+
+        axios.get(url)
+        .then(response => {
+          setCartItems(response.data.data);
+          calculateCheckout();
+        })
+        .catch(error => {
+          console.log(error)
+        })
+      }
+    }
+  }
+
+  function calculateCheckout() {
+    let items = 0;
+    let discounts = 0;
+    let subtotal = 0;
+
+    if (cartItems) {
+      cartItems.forEach(function(item) {
+        items += item.attributes.last_price;
+        discounts += item.attributes.current_price - item.attributes.last_price;
+        subtotal += item.attributes.current_price;
+      });
+
+      setCheckout({
+        items: items,
+        discounts: discounts,
+        subtotal: subtotal
+      });
+    } else {
+      setCheckout(null);
+    }
+  }
+
+  const removeItemCart = (id) => {
+    if (cartItems) {
+      let updatedCartItems = cartItems.filter(function(item) {
+        return item.id != id;
+      });
+      let sessionCart = sessionStorage.getItem("cart");
+      if (sessionCart) {
+        let cart = JSON.parse(sessionCart);
+        if (sessionCart.length) {
+          let updatedSessionCart = cart.filter(function(item) {
+            return item.id != id;
+          })
+          sessionStorage.setItem("cart", JSON.stringify(updatedSessionCart));
+        }
+      }
+      setCartItems(updatedCartItems);
+      calculateCheckout();
+    }
+  }
+
   return (
     <>
       <Navbar></Navbar>
       <div className="pl-44 pr-44 pt-5 pb-10 mt-20">
         <h3 className="text-2xl ml-36 font-bold mb-7">Meu carrinho</h3>
-        <div className="flex">
-          <div className="w-3/4 max-h-96 mr-5 overflow-y-scroll">
-            <CardGameResume></CardGameResume>
-            <CardGameResume></CardGameResume>
-            <CardGameResume></CardGameResume>
-          </div>
-          <div className="bg-stone-950 flex flex-col justify-between h-auto w-1/4 p-5 rounded-xl">
-            <h4 className="font-semibold text-lg">Resumo do pedido</h4>
-            <div>
-              <p className="text-sm flex justify-between">Itens: <strong className="font-medium">R$750,80</strong></p>
-              <p className="text-sm flex justify-between">Descontos: <strong className="font-medium text-lime-500">-R$300,32</strong></p>
-              <p className="text-lg mt-2 flex justify-between">Total: <strong>R$450,48</strong></p>
-              <button className="bg-blue-600 w-full pt-2 pb-2 mt-5 text-center rounded-lg hover:bg-blue-500">Finalizar</button>
+        {
+          cartItems && checkout && (
+            <div className="flex">
+              <div className="w-3/4 mr-5">
+                {
+                  cartItems.map(item => {
+                    return (
+                      <CardGameResume key={item.id} game={item} removeItemCart={removeItemCart}></CardGameResume>
+                    )
+                  })
+                }
+              </div>
+              <div className="bg-stone-950 flex flex-col justify-between h-96 w-1/4 p-5 rounded-xl">
+                <h4 className="font-semibold text-lg">Resumo do pedido</h4>
+                <div>
+                  <p className="text-sm flex justify-between">Itens: <strong className="font-medium">{ utils.getMonetaryFormat(checkout.items) }</strong></p>
+                  <p className="text-sm flex justify-between">Descontos: <strong className="font-medium text-lime-500">{ utils.getMonetaryFormat(checkout.discounts) }</strong></p>
+                  <p className="text-lg mt-2 flex justify-between">Subtotal: <strong>{ utils.getMonetaryFormat(checkout.subtotal) }</strong></p>
+                  <button className="bg-blue-600 w-full pt-2 pb-2 mt-5 text-center rounded-lg hover:bg-blue-500">Finalizar compra</button>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          )
+        }
       </div>
       <Footer></Footer>
     </>
